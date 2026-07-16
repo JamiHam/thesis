b5be617c25ff0be73903caffdce05f52f3f09cd1
JamiHam <jami.hamala@gmail.com>
2026-07-16 11:30:00 +0300

    Added learning rate scheduling, changed the way results are printed and saved

--- software/code/engine.py
+++ software/code/engine.py
@@ -3,12 +3,17 @@ from pathlib import Path
 import torch
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
+from timeit import default_timer as timer
+import time
+from utils import save_to_file
 
-def save_training_results(output_directory: Path,
-                          model_name: str,
-                          results: Dict,
-                          epoch: int,
-                          learning_rate: float):
+def save_epoch_results(output_directory: Path,
+                       model_name: str,
+                       results: Dict,
+                       epoch: int,
+                       learning_rate: float):
+    
+    formatted_learning_rate = str(learning_rate).rstrip('0')
     
     results_string = (
         f'Epoch: {epoch} | '
@@ -16,19 +21,53 @@ def save_training_results(output_directory: Path,
         f'train_accuracy: {results["train_accuracy"][-1]:.2f}% | '
         f'validation_loss: {results["validation_loss"][-1]:.4f} | '
         f'validation_accuracy: {results["validation_accuracy"][-1]:.2f}% | '
-        f'learning_rate: {learning_rate:.6f}'
+        f'learning_rate: {formatted_learning_rate}'
     )
 
-    target_directory = output_directory / 'training-results'
-    if not os.path.exists(target_directory):
-        os.makedirs(target_directory)
-
-    file_path = target_directory / model_name
-    with open(file_path, 'a', encoding='utf-8') as file:
-        file.write(results_string)
-        file.write('\n')
+    directory = output_directory / 'training-results'
+    file_name = f'{model_name}.txt'
+    
+    save_to_file(directory=directory,
+                 file_name=file_name,
+                 content=results_string)
 
     return results_string
+
+def save_training_results(output_directory: Path,
+                          model_name: str,
+                          start_time: float,
+                          end_time: float,
+                          epochs: int,
+                          lowest_validation_loss: float,
+                          best_epoch: int):
+    
+    total_seconds = end_time - start_time
+    total_time = time.strftime('%H:%M:%S', time.gmtime(total_seconds))
+    seconds_per_epoch = total_seconds / epochs
+
+    results_string = (
+        f'Total training time: {total_time} | '
+        f'Time per epoch: {seconds_per_epoch:.2f} seconds | '
+        f'Lowest validation loss: {lowest_validation_loss:.4f} | '
+        f'Best epoch: {best_epoch}'
+    )
+
+    directory = output_directory / 'training-results'
+    file_name = f'{model_name}.txt'
+
+    save_to_file(directory=directory,
+                 file_name=file_name,
+                 content=results_string)
+    
+    return results_string
+
+def save_model(model: torch.nn.Module,
+               model_name: str,
+               model_directory: Path):
+    
+    model_path = f'{model_directory}/{model_name}.pth'
+    torch.save(obj=model.state_dict(),
+                f=model_path)
 
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
@@ -99,7 +138,8 @@ def train(model: torch.nn.Module,
           device: torch.device,
           model_directory: Path,
           output_directory: Path,
-          model_name: str) -> Dict[str, List]:
+          model_name: str,
+          minimum_epochs: int) -> Dict[str, List]:
     
     results = {
         'train_loss': [],
@@ -109,6 +149,9 @@ def train(model: torch.nn.Module,
     }
 
     lowest_validation_loss = 1
+    best_epoch = 0
+
+    start_time = timer()
 
     for epoch in tqdm(range(epochs)):
         train_loss, train_accuracy = train_step(model=model,
@@ -117,31 +160,44 @@ def train(model: torch.nn.Module,
                                                 optimizer=optimizer,
                                                 device=device)
         
+        results['train_loss'].append(train_loss)
+        results['train_accuracy'].append(train_accuracy)
+        
         validation_loss, validation_accuracy = validation_step(model=model,
                                                                dataloader=validation_dataloader,
                                                                loss_function=loss_function,
                                                                device=device)
         
-        scheduler.step(validation_loss)
-
-        results['train_loss'].append(train_loss)
-        results['train_accuracy'].append(train_accuracy)
         results['validation_loss'].append(validation_loss)
         results['validation_accuracy'].append(validation_accuracy)
+        
+        if epoch >= minimum_epochs:
+            scheduler.step(validation_loss)
 
-        print(save_training_results(output_directory=output_directory,
-                                    model_name=model_name,
-                                    results=results,
-                                    epoch=epoch + 1,
-                                    learning_rate=optimizer.param_groups[0]['lr']))
+        print(save_epoch_results(output_directory=output_directory,
+                                 model_name=model_name,
+                                 results=results,
+                                 epoch=epoch + 1,
+                                 learning_rate=optimizer.param_groups[0]['lr']))
         
         if validation_loss < lowest_validation_loss:
             print(f'Lowest validation loss so far ({validation_loss:.4f}), saving model...')
 
-            model_path = f'{model_directory}/{model_name}.pth'
-            torch.save(obj=model.state_dict(),
-                       f=model_path)
+            save_model(model=model,
+                       model_name=model_name,
+                       model_directory=model_directory)
             
             lowest_validation_loss = validation_loss
+            best_epoch = epoch + 1
+
+    end_time = timer()
+
+    print(save_training_results(output_directory=output_directory,
+                                model_name=model_name,
+                                start_time=start_time,
+                                end_time=end_time,
+                                epochs=epochs,
+                                lowest_validation_loss=lowest_validation_loss,
+                                best_epoch=best_epoch))
 
     return results
