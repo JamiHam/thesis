5217f7ba4351b8a79bc5c87a747a48cbdc8a3d0e
JamiHam <jami.hamala@gmail.com>
2026-07-16 11:37:35 +0300

    Changed the way images are preprocessed to match the other scripts, fixed incompatibility with Swin Transformer

--- software/code/grad_cam.py
+++ software/code/grad_cam.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import torch
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from torchvision import datasets
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
 import model_setup
-from utils import read_config, generate_file_path
+from utils import read_config, generate_file_path, create_model_summary
+from PIL import Image
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 config = read_config()
@@ -19,7 +19,6 @@ output_directory = Path(config['output_directory'])
 
 model_type = config['model']
 model_name = config['model_name']
-target_epoch = config['target_epoch']
 
 def predict(model, image_tensor, class_names):
     model.eval()
@@ -41,33 +40,41 @@ def predict(model, image_tensor, class_names):
 
 # Get the last convolutional layer of the model
 def get_target_layer(model, model_type):
-    if model_type == 'convnext':
-        return model.features[-1]
-    
     if model_type == 'resnet':
-        return model.layer4
+        if config['resnet_version'] == 1:
+            return model.layer4
+        else:
+            return model.norm
     
     if model_type == 'swin_transformer':
-        return model.features[-1][-1].norm2
+        return model.norm
+    
+    if model_type == 'convnext':
+        return model.features[-1]
+
+# Swin Transformer's outputs use a different order for their dimensions where the feature channels are placed last.
+# This function rearranges the dimensions to match the other models.
+def reshape_transform(tensor):
+    if model_type == 'swin_transformer':
+        return tensor.permute(0, 3, 1, 2)
+    else:
+        return tensor
 
 def create_cam_image(model, image, image_tensor):
     target_layers = [get_target_layer(model, model_type)]
+    image = np.float32(image) / 255
 
-    with GradCAM(model=model, target_layers=target_layers) as cam:
+    with GradCAM(model=model, target_layers=target_layers, reshape_transform=reshape_transform) as cam:
         grayscale_cam = cam(input_tensor=image_tensor)
         grayscale_cam = grayscale_cam[0, :]
         cam_image = show_cam_on_image(image, grayscale_cam, use_rgb=True)
-        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
 
     return cam_image
 
-def display_cam_image(cam_image, title):
-    cam_image = cv2.cvtColor(cam_image, cv2.COLOR_BGR2RGB)
-    original_image = cv2.imread(image_path)
+def display_cam_image(image, cam_image, title):
+    fig, ax = plt.subplots(1, 2, figsize=(10, 6))
 
-    fig, ax = plt.subplots(1, 2, figsize=(10, 4))
-
-    ax[0].imshow(original_image)
+    ax[0].imshow(image)
     ax[0].axis('off')
     ax[1].imshow(cam_image)
     ax[1].axis('off')
@@ -88,29 +95,33 @@ def save_figure(figure):
 
 def main():
     class_names = datasets.ImageFolder(data_directory).classes
-    model, preprocess = model_setup.setup(model=model_type,
+    model, preprocess = model_setup.setup(model=config['model'],
                                           pretrained=False,
                                           class_names=class_names,
                                           device=device,
                                           size=config['size'],
                                           resnet_layers=config['resnet_layers'],
+                                          resnet_version=config['resnet_version'],
                                           swin_transformer_version=config['swin_transformer_version'])
     
-    model_path = f"{model_directory}/{model_name}/{model_name}_epoch{target_epoch}.pth"
+    model_path = f'{model_directory}/{model_name}.pth'
     model.load_state_dict(torch.load(f=model_path,
                                      map_location=device))
     
-    image = cv2.imread(image_path, 1)[:, :, ::-1]
-    image = np.float32(image) / 255
-    image_tensor = preprocess_image(image,
-                                    mean=preprocess.mean,
-                                    std=preprocess.std).to(device)
+    image = Image.open(image_path)
+    image_tensor = preprocess(image).unsqueeze(0).to(device)
+    resized_image = image.resize((224, 224))
     
     results = predict(model, image_tensor, class_names)
     title = f'Predicted label: {results['predicted_label']}\nConfidence {results['confidence']}'
+
+    cam_image = create_cam_image(model=model,
+                                 image=resized_image,
+                                 image_tensor=image_tensor)
     
-    cam_image = create_cam_image(model, image, image_tensor)
-    display_cam_image(cam_image, title)
+    display_cam_image(image=resized_image, 
+                      cam_image=cam_image,
+                      title=title)
 
 if __name__ == '__main__':
     main()
